efbd8bbdcdfc241a0ec160bb9f938f7ccb882a48
Dominik Lasch <dl@exutec.de>
2017-11-20 16:13:18 +0100

    done 404, 500, Homepage and added views, post-model (not done yet)

--- app.js
+++ app.js
@@ -81,20 +81,20 @@ app.use('/feed', feed);
 // --404 AND ERROR HANDLER--//
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status(404);
+  res.render('not-found');
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
 });
 
 module.exports = app;
